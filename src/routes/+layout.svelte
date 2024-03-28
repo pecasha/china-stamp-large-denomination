733d74e8e6d4ec90f53d53d3b88b314f8857280a
pecasha <chihai@pecasha.com>
2024-03-28 13:13:37 +0800

    :sparkles: 支持PWA

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<slot></slot>
+
+<svelte:head>
+    {@html webManifestLink}
+</svelte:head>
+
+<script lang="ts">
+    import { pwaInfo } from "virtual:pwa-info";
+
+    $: webManifestLink = pwaInfo?.webManifest.linkTag || "";
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,38 @@
-import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+
+import { sveltekit } from "@sveltejs/kit/vite";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 
 export default defineConfig({
     plugins: [
-        sveltekit()
+        sveltekit(),
+        SvelteKitPWA({
+            registerType: "autoUpdate",
+            includeAssets: [
+                "favicon.png"
+            ],
+            workbox: {
+                globPatterns: [
+                    "**/*.{jpg,png}"
+                ]
+            },
+            manifest: {
+                name: "中国大面值邮票图鉴",
+                short_name: "大面值邮票",
+                description: "收录 2002年9月7日 之后发行的面值大于等于 2元 的邮票，不包含小型张",
+                background_color: "#f2f2f2",
+                theme_color: "#d6204b",
+                display: "standalone",
+                orientation: "landscape-primary",
+                icons: [
+                    {
+                        src: "favicon.png",
+                        sizes: "128x128",
+                        type: "image/png"
+                    }
+                ]
+            }
+        })
     ],
     server: {
         host: "0.0.0.0"
